62280bde7c149eddcbad579ca8a8c433d23694f7
kolyana31 <kolyana31@gmail.com>
2020-10-04 11:14:05 +0300

    buscket add

--- js/catalog.js
+++ js/catalog.js
@@ -32,10 +32,10 @@ function CreateProduct(id,name,price,stars,photo){
             </svg>
         </div>
         <div class="value d-flex align-items-center">
-            <span id="popular-price-0">${price}</span>$
+            <span id="popular-price-${id}">${price}</span>$
         </div>
         <div class="d-flex button-block text-dark">
-            <button id="add-${id}" type="button" class="btn btn-primary w-100 d-flex align-items-center justify-content-center" data-toggle="button" aria-pressed="false">
+            <button name="AddToBucket" AddId="${id}" type="button" class="btn btn-primary w-100 d-flex align-items-center justify-content-center" data-toggle="button" aria-pressed="false">
                 Add to Cart
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M5.5 3.5a2.5 2.5 0 0 1 5 0V4h-5v-.5zm6 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/>
@@ -43,9 +43,31 @@ function CreateProduct(id,name,price,stars,photo){
             </button>
         </div>
     `
+    Prod.addEventListener("click", (e)=>{
+        let id;
+        if(e.target.getAttribute("AddId")){
+            id = e.target.getAttribute("AddId");
+        } 
+        else{
+            id = (e.target.parentNode).parentNode.getAttribute("AddId");
+        }
+        if (localStorage.getItem("busket")) {
+            let recop = []
+            recop =  JSON.parse(localStorage.getItem("busket"));
+            recop.push(id);
+            localStorage.setItem("busket",JSON.stringify(recop));
+        }
+        else{
+            let st = [];
+            st.push(id);
+            localStorage.setItem("busket",JSON.stringify(st));
+        }
+    })
+
     return Prod;
 }
 
+document.querySelectorAll("button[name=AddToBucket]")
 
 function GetFilters() {
     let Result;
